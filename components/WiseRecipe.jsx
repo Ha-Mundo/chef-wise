7115955699cf862f89dc2de56caa0a9a6e29bb76
Ha-Mundo <trauzzu@gmail.com>
2025-09-03 17:20:58 +0100

    Added ReactMarkdown component to convert Markdown to regular HTML

--- components/WiseRecipe.jsx
+++ components/WiseRecipe.jsx
@@ -1,7 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+
 export default function WiseRecipe(props) {
-    return (
-        <section>
-               {props.recipe}
-            </section>
+   return (
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>👨‍🍳Chef Wise Recommends:</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
+        </section>
     )
 }
