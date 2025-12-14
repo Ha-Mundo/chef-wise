b14033fe611874c38124cf43bde4ededec5ef6de
Ha-Mundo <trauzzu@gmail.com>
2025-12-14 12:25:51 +0000

    image file name updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
-import chefClaudeLogo from "../images/chef-claude-icon.png"
+import chefWiseLogo from "../images/chef-wise-icon.png"
 
 export default function Header() {
     return (
         <header>
-            <img src={chefClaudeLogo}/>
+            <img src={chefWiseLogo}/>
             <h1>Chef Wise</h1>
         </header>
     )
